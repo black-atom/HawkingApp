4cc2890a320525cd12d35f257be5a26832eca462
vitorsilvalima <vitor.lima2@fatec.sp.gov.br>
2018-03-25 22:38:14 -0300

    redux: add persistance for reducers

--- src/redux/redux.module.ts
+++ src/redux/redux.module.ts
@@ -1,18 +1,26 @@
 import { ProvidersModule } from './../providers/providers.module';
 import { NgModule } from '@angular/core';
-
-import { StoreModule } from '@ngrx/store';
+import { StoreModule, ActionReducer } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { EffectsModule } from '@ngrx/effects';
+import { localStorageSync } from 'ngrx-store-localstorage';
 
 import { reducers } from './reducers';
 import { effects } from './effects';
+
+function localStorageSyncReducer(reducer: ActionReducer<any>): ActionReducer<any> {
+  return localStorageSync({
+    keys: ['login','atendimentos', 'monitoramentos'],
+    rehydrate: true,
+  })(reducer);
+}
+const metaReducers: [any] = [localStorageSyncReducer];
 
 @NgModule({
   imports:[
     ProvidersModule,
     EffectsModule.forRoot(effects),
-    StoreModule.forRoot(reducers),
+    StoreModule.forRoot(reducers, { metaReducers }),
     StoreDevtoolsModule.instrument({ maxAge: 5 }),
   ],
 })
