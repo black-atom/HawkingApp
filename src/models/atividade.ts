5cd9210e497f468e06882f8423bf6013a068c176
vitorsilvalima <vitor.lima2@fatec.sp.gov.br>
2018-04-10 00:18:01 -0300

    models: refactor models according to new schema

--- src/models/atividade.ts
+++ src/models/atividade.ts
@@ -1,53 +1,18 @@
-import { Action } from './monitoramento';
-import {
-  Cliente,
-  Endereco,
-  Contato,
-  Tecnico,
-  Avaliacao,
-  InteracaoTecnico,
-  Motivo,
-  Assinatura,
-} from './atendimento';
+import { Monitoramento } from './monitoramento';
 
+export enum AtividadeTipo {
+  almoco = 'almoco',
+  atendimento = 'atendimento',
+  deslocamento_empresa = 'deslocamento_empresa',
+  abastecimento = 'abastecimento',
+  outros = 'outros',
+}
 
-export interface IAtividade {
-  _id: string;
-  cliente: Cliente;
-  endereco: Endereco;
-  contato: Contato;
-  tecnico: Tecnico;
-  avaliacao: Avaliacao[];
-  data_atendimento: Date;
-  estacionamento: string;
-  modelo_equipamento: string;
-  numero_equipamento: string;
-  tipo: string;
-  descricao: string;
-  testes_efetuados: string;
-  observacao: string;
-  valor: string;
-  autorizado: string;
-  estado: string;
-  interacao_tecnico: InteracaoTecnico;
-  motivos: Motivo[];
-  createdBy: string;
-  createdAt: string;
-  updatedAt: string;
-  updatedBy: string;
-  imagens: any[];
-  synced?: boolean;
-  assinatura: Assinatura;
-  uuid?: string;
-  km_inicial?: number;
-  km_final?: number;
-  data_hora_inicial_km?: Date;
-  data_hora_final_km?: Date;
-  data_hora_inicial_virgente_local?: Date;
-  data_hora_final_virgente_local?: Date;
-  id_funcionario?: string;
-  id_atendimento?: string;
-  isUploaded?: boolean;
-  isPaused?: boolean;
-  actions?: Action[];
+export interface AtividadeI {
+  atividade_id: String;
+  isPaused: Boolean;
+  monitoramentos: [Monitoramento];
+  tipo: AtividadeTipo;
+  funcionario_id: String;
+  atendimento_id: String;
 }
--- src/models/monitoramento.ts
+++ src/models/monitoramento.ts
@@ -1,23 +1,8 @@
 export interface Monitoramento {
-  uuid?: string;
-  _id?: string;
-  km_inicial?: number;
-  km_final?: number;
-  data_hora_inicial_km?: Date;
-  data_hora_final_km?: Date;
-  data_hora_inicial_virgente_local?: Date;
-  data_hora_final_virgente_local?: Date;
-  tipo?: string;
-  id_funcionario?: string;
-  id_atendimento?: string;
-  isUploaded?: boolean;
-  isPaused?: boolean;
-  descricao: string;
-  actions?: Action[];
-}
-
-export interface Action {
-  status?: string;
-  motivo?: string;
-  date?: Date;
+  start_time?: Date;
+  end_time?: Date;
+  dislocation_time?: Date;
+  arrival_time?: Date;
+  paused_time?: Date;
+  motivo?: String;
 }
