63a3dedc20e8b6dd73c583bf3103d92377769e06
alexandrejuk <ale_santos.soares@hotmail.com>
2018-04-12 14:22:25 -0300

    add native-camera to app and create method changePicture

--- src/pages/atividades/components/foto/foto.ts
+++ src/pages/atividades/components/foto/foto.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera';
+
 
 @Component({
   selector: 'foto',
@@ -7,6 +9,66 @@ import { Component } from '@angular/core';
 @IonicPage({
   name: 'FotoPage',
 })
+
 export class FotoPage {
-  constructor() { }
+
+
+  private options: CameraOptions = {
+    quality: 100,
+    destinationType: this.camera.DestinationType.DATA_URL,
+    encodingType: this.camera.EncodingType.JPEG,
+    mediaType: this.camera.MediaType.PICTURE,
+    saveToPhotoAlbum: true,
+    cameraDirection: 0,
+    correctOrientation: true,
+  };
+
+  public photosStart = [
+    {
+      id: 1,
+      tipo: 'inicio',
+      src: 'assets/imgs/img-default.png',
+    },
+    {
+      id: 2,
+      tipo: 'inicio',
+      src: 'assets/imgs/img-default.png',
+    },
+    {
+      id: 3,
+      tipo: 'inicio',
+      src: 'assets/imgs/img-default.png',
+    },
+  ];
+  public photosEnd = [
+    {
+      id: 1,
+      tipo: 'fim',
+      src: 'assets/imgs/img-default.png',
+    },
+    {
+      id: 2,
+      tipo: 'fim',
+      src: 'assets/imgs/img-default.png',
+    },
+    {
+      id: 3,
+      tipo: 'fim',
+      src: 'assets/imgs/img-default.png',
+    },
+
+  ];
+
+  constructor(private camera: Camera) {
+
+  }
+
+
+
+  changePicture(tipo) {
+    this.camera.getPicture(this.options).then((imageData) => {
+      const base64Image = 'data:image/jpeg;base64,' + imageData;
+      console.log(base64Image);
+    });
+  }
 }
