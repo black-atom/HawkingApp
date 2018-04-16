9a874b8720ddf0a82a85f08d51aec497285702a6
alexandrejuk <ale_santos.soares@hotmail.com>
2018-04-16 13:40:20 -0300

    create file to alert mock

---
+++ src/utils/AlertInputAtividade.ts
@@ -0,0 +1,18 @@
+const configAlertInputAtividade = {
+  descricao: {
+    name: 'descricao',
+    title: 'Descrição da Atividade',
+    message: 'Digite a descrição da atividade',
+    type: 'descricao',
+  },
+  cancelar: {
+    name: 'cancelar',
+    title: 'Cancelar Atividade',
+    message: 'Motivo do cancelamento',
+    type: 'cancelar',
+  },
+};
+
+export {
+  configAlertInputAtividade,
+};
